5d59647e2302603231fbe04e79a310c75fe2420e
cloutierm <cloutierm@educ.college-montreal.qc.ca>
2023-09-22 09:57:41 -0400

    tp1 dtgf  yhtd g

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #Code du TP 1
 
 
-
+#Voici la fonction
 def count_words(phrase): #creation de la fonction count_words
     nmb_mots = len(phrase .split()) #ligne qui va compter le nombre de mots
     return nmb_mots  #on return la variable
